1dce7cd3c87b39fb123a4794278cc386ab6f93ed
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-13 22:04:58 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,345 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Glass Salt and Pepper Shakers',
+        price: 5.95,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Strong, Sturdy, High Quality And Stylish.',
+            },
+            {
+                text: 'Great For Any Event.',
+            },
+            {
+                text: 'Durable For Daily Use.',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: '60-Inch Lightweight Tripod',
+        price: 19.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Lightweight tripod with adjustable-height legs and rubber feet.',
+            },
+            {
+                text: 'Compatible with most video cameras, digital cameras, still cameras, GoPro devices, smartphone adapters (not included), and scopes.',
+            },
+            {
+                text: 'Recommended max load weight is 6.6 lbs (3kg) for optimal performance.',
+            },
+            {
+                text: 'Weighs 3 lbs; Extends from 25 inches to 60 inches when center post is fully extended.',
+            },
+            {
+                text: 'Two built-in bubble view levels and 3-way head to allow for tilt and swivel motion; portrait or landscape options.',
+            },
+            {
+                text: 'Quick-release mounting plate helps ensure fast transitions between shots.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Eye-caring LED Desk Lamp',
+        price: 24.99,
+        discount: 14.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Modern table or desk lamp for home or office.',
+            },
+            {
+                text: 'Non-glaring, soft illumination that\'s easy on your eyes; 32 energy-efficient LED lights.',
+            },
+            {
+                text: 'Toggle between 3 color modes: natural light, white light, and yellow light.',
+            },
+            {
+                text: 'Lightweight and portable design; easy to move to different areas of the house or office.',
+            },
+            {
+                text: '40 minute timer, dimmable sensor touch, and sensor touch on/off switch.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.jpg'],
+    },
+    {
+        id: 'product-4',
+        name: 'Champagne Flute Glasses Set of 4',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Enjoy your wine in the most comfortable and stylish %100 lead free stemless wine glasses at almost every special occasion, people choose to share a fine glass of wine.',
+            },
+            {
+                text: 'Made of glass enabling the color of the wine to shine. Elegant look and adds to any table\'s aesthetics with their contemporary design. Increase the overall drinking experience and is strongly recommended by health experts.',
+            },
+            {
+                text: 'Stemless glasses are eye-catching. They are innovative in design, style, and form. Ideal for contemporary bars or stylish restaurants.',
+            },
+            {
+                text: 'Shaped to be an all-purpose glass the base of this glass provides maximum stability. Ideal for wine and all beverages as well. Breakthrough styling and unmatched brand value. Unique Glass shape enhances the juices aromas and flavors.',
+            },
+            {
+                text: 'The base of this glass provides maximum stability. Ideal for juice and all beverages as well.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.jpg'],
+    },
+    {
+        id: 'product-5',
+        name: 'Medical Back Brace Lumbar Support Belt',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Stop back pain',
+                text: 'With this lumbar support belt that has double pull strap closures; our lumbar support brace lets you find the right combination of comfort and back support to ease your back pain; This is a lightweight back brace for men and women.',
+            },
+            {
+                title: 'Back braces for lower back pain',
+                text: 'Back braces come in multiple sizes. Back brace belt offers maximum lumbar support and abdominal area support. The improved posture and proper support promoted by a back brace belt can help provide lower back pain relief.',
+            },
+            {
+                title: 'Save your back',
+                text: 'Our orthopedic braces are made from lightweight breathable material; lumbar back brace offers comfortable support and can accommodate many different body types. We carry a variety of back braces in a range of sizes for women and men.',
+            },
+            {
+                title: 'Lower back brace',
+                text: 'That will supply lower back pain relief while promoting correct spine alignment & posture. Brace contains 2 pockets for adding a hot cold therapy pack.',
+            },
+            {
+                title: 'Quality support and comfort',
+                text: 'Back support brace belts offer customizable support fit and comfortability that protects against injury during demanding activities.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Classic Numbered Wall Clock',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Round silver case, white face with black numerals.',
+            },
+            {
+                text: 'Silver quiet sweep second hand.',
+            },
+            {
+                text: 'Glass face.',
+            },
+            {
+                text: 'Powered by 1 AA battery (included).',
+            },
+            {
+                text: 'Overall dimensions: 14 X 1.6',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Training Gloves',
+        price: 49.95,
+        discount: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: '80% Polyurethane/20% Polyester',
+            },
+            {
+                text: 'The Package Height Of The Product Is 4.9 Inches',
+            },
+            {
+                text: 'The Package Length Of The Product Is 14.3 Inches',
+            },
+            {
+                text: 'The Package Width Of The Product Is 7.5 Inches',
+            },
+            {
+                text: 'Sport type: Boxing',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Mechanics Socket Set',
+        price: 69.99,
+        discount: 69.94,
+        image: './img/img8.png',
+        description: [
+            {
+                text: '145-Piece Socket Set',
+            },
+            {
+                text: 'Chrome Vanadium forged body for torque, strength and durability',
+            },
+            {
+                text: 'Quick release switch and reversible mechanism for easy operation',
+            },
+            {
+                text: 'Full polished chrome finish socket and wrench for anti-corrosion protection',
+            },
+            {
+                text: 'Combine the most popular tools for easy accessibility',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png', './img/img8-3.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Countertop Blender',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                title: 'Professional power',
+                text: '1000 watts of professional power can crush ice and breakdown any tough ingredient.',
+            },
+            {
+                title: 'XL capacity',
+                text: 'The 72 oz professional blender pitcher is excellent for making frozen drinks and creamy smoothies for the entire family. 64 oz. max liquid capacity.',
+            },
+            {
+                title: 'Intelligent technology',
+                text: 'Total Crushing Technology delivers unbeatable power with blades that pulverize and crush through ice, whole fruits and vegetables in seconds.',
+            },
+            {
+                title: 'Ice crushing',
+                text: 'Blast ice into snow in seconds and blend your favorite ingredients into delicious resort-style frozen drinks.',
+            },
+            {
+                title: 'Recipe inspiration',
+                text: 'The included recipe guide provides drink and dish making inspiration.',
+            },
+            {
+                title: 'Easy cleanup',
+                text: 'Pitcher is BPA free and dishwasher safe, making cleanup just as simple and easy as using the blender.',
+            },
+            {
+                title: 'What\'s included',
+                text: '1000 Watt Motor Base Professional Blender & 72 oz Total Crushing Pitcher with Lid.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Folding Wagon for kids and cargo',
+        price: 84.41,
+        discount: 95.50,
+        image: './img/img10.png',
+        description: [
+            {
+                title: '3 wagons in 1',
+                text: 'This collapsible wagon has 3 riding features: hauling, 2 rider seating and bench seating. Easily transform from hauling to bench seating with the zippers on the side of the wagon.',
+            },
+            {
+                title: 'Folds for storage',
+                text: 'Easy one hand fold allows convenient storage. This wagon is portable and can be stored or taken anywhere! Folded wagon dimensions: 7” L x 18.5” W x 32.68” H',
+            },
+            {
+                title: 'Safe for kids',
+                text: 'Wagons pass the ASTM-f963-13 toy safety standard. This wagon includes seat belts and a padded seat.',
+            },
+            {
+                title: 'Storage capacity',
+                text: 'The wagon can haul up to 150 lbs. of kids or cargo! It also includes a rear storage loops and two front cup holders.',
+            },
+            {
+                title: 'Specifications',
+                text: 'Assembled product dimensions: 39.4” L x 21.2” W x 32.7” H. Recommended for kids 1 ½+ years. Maximum weight capacity 150 lbs',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Step Can',
+        price: 89.99,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                text: 'Print & stain-free, satin finish stainless steel is easy to maintain.',
+            },
+            {
+                text: 'Heavy-duty hinge construction and durable pedal provides easy step open access.',
+            },
+            {
+                text: 'Durable pedal provides easy step-open access.',
+            },
+            {
+                text: 'Soft-closing lid.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Fishfinder with Traditional Transducer',
+        price: 109.00,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'Scanning Sonar shows you more of what is in the water around your boat; This high frequency sonar gives near photographic images with detailed representations of objects, structure and fish.',
+            },
+            {
+                text: 'The power of simple offers a keyed interface with dedicated buttons; The device is easy to use and easy to install; Available in 3.5, 5 and 7 inch display sizes; Water rating IPX7',
+            },
+            {
+                text: 'Waypoint map: Use the way point map to easily view, mark and navigate to locations such as brush piles, stumps and docks; Maximum depth 1,600 feet freshwater, 750 feet saltwater; Current draw at 12 volt: 0.23 Ampere.',
+            },
+            {
+                text: 'Sonar sends a continuous sweep of frequencies which provides a wider range of information; Sonar is able to create crisper fish arches with better target separation.',
+            },
+            {
+                text: 'Built in flasher: View your sonar data in the classic flasher format; Ideal for ice fishing or vertical jigging. Display size: 1.9 x 2.9 inches (4.9 x 7.3 centimeter); 3.5 inches diagonal (8.7 centimeter).',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.png', './img/img12-3.png'],
+    },
+    {
+        id: 'product-13',
+        name: 'Knife collection',
+        price: 119.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                text: 'Superior high-carbon stainless steel blades for precision and accuracy.',
+            },
+            {
+                text: 'Precision-tapered ground blades that narrow gradually to an extremely sharp, fine edge.',
+            },
+            {
+                text: 'Forged durable extra-wide safety bolster for perfect stability and control.',
+            },
+            {
+                text: 'Stainless steel rivets to secure the full-tang blade to the ergonomically designed handle.',
+            },
+        ],
+        images: ['./img/img13-1.png', './img/img13-2.png', './img/img13-3.png'],
+    },
+]
